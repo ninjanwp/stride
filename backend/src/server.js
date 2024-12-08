f53fe81e970ea576f18cc4ae6106f53800760d49
Nick <nick.pfef@gmail.com>
2024-12-07 22:21:12 -0500

    fresh start

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const db = require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
